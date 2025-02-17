7f50662e785d5516f51680b9ae8eeb39a8aed587
forcedcoding <shashwatar.9876@gmail.com>
2025-02-17 14:39:19 +0530

    more modified

--- Day3/func.js
+++ Day3/func.js
@@ -8,3 +8,16 @@ const S1 =(a, b) => {
 console.log(S1(2, 3));
 const summmm = (a, b) => (a + b) % 10;
 console.log(summmm(2, 3));
+
+const sumMod10 = (a, b) => {
+    const ans = (a + b) % 10;
+    const printPretty = (txt) => {
+        console.log("-------",(txt),"-------");
+    };
+    printPretty(ans);
+    console.log("Sum done");
+}
+sumMod10(2, 3);
+console.log("Done");
+
+console.log("-------");
