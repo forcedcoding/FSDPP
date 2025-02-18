363f13b8a630d5d0a1a138521ce1d8ce2423c5de
forcedcoding <shashwatar.9876@gmail.com>
2025-02-18 13:41:39 +0530

    day4 started

---
+++ Day4/new1.js
@@ -0,0 +1,19 @@
+const sum = (a,b) => {
+    const ans = a + b;
+    return ans;
+};
+
+const printPretty = (txt) => {
+    console.log("----------------------");
+    console.log(txt);
+    console.log("----------------------");
+};
+
+sum(2,3);
+console.log(sum(2,3));
+console.log(printPretty("Hello World!"));
+printPretty("Hello World!");
+// What is the output of this code?
+// a) 5
+// b) Hello World!
+// c) ----------------------
